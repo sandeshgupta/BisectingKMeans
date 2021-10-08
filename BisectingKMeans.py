3327668ae380362381dafb4df7eec1098e988b7f
Sandesh Gupta <sandesh.gupta93@gmail.com>
2021-10-08 03:19:46 -0700

    Accuracy - 66%

--- BisectingKMeans.py
+++ BisectingKMeans.py
@@ -3,6 +3,69 @@ from collections import Counter
 from scipy.sparse import csr_matrix
 from collections import defaultdict
 
+
+def csr_info(mat, name="", non_empy=False):
+    r""" Print out info about this CSR matrix. If non_empy, 
+    report number of non-empty rows and cols as well
+    """
+    if non_empy:
+        print("%s [nrows %d (%d non-empty), ncols %d (%d non-empty), nnz %d]" % (
+                name, mat.shape[0], 
+                sum(1 if mat.indptr[i+1] > mat.indptr[i] else 0 
+                for i in range(mat.shape[0])), 
+                mat.shape[1], len(np.unique(mat.indices)), 
+                len(mat.data)))
+    else:
+        print( "%s [nrows %d, ncols %d, nnz %d]" % (name, 
+                mat.shape[0], mat.shape[1], len(mat.data)) )
+        
+# # scale matrix and normalize its rows
+# def csr_idf(mat, copy=False, **kargs):
+#     r""" Scale a CSR matrix by idf. 
+#     Returns scaling factors as dict. If copy is True, 
+#     returns scaled matrix and scaling factors.
+#     """
+#     if copy is True:
+#         mat = mat.copy()
+#     nrows = mat.shape[0]
+#     nnz = mat.nnz
+#     ind, val, ptr = mat.indices, mat.data, mat.indptr
+#     # document frequency
+#     df = defaultdict(int)
+#     for i in ind:
+#         df[i] += 1
+#     # inverse document frequency
+#     for k,v in df.items():
+#         df[k] = np.log(nrows / float(v))  ## df turns to idf - reusing memory
+#     # scale by idf
+#     for i in range(0, nnz):
+#         val[i] *= df[ind[i]]
+        
+#     return df if copy is False else mat
+
+# def csr_l2normalize(mat, copy=False, **kargs):
+#     r""" Normalize the rows of a CSR matrix by their L-2 norm. 
+#     If copy is True, returns a copy of the normalized matrix.
+#     """
+#     if copy is True:
+#         mat = mat.copy()
+#     nrows = mat.shape[0]
+#     nnz = mat.nnz
+#     ind, val, ptr = mat.indices, mat.data, mat.indptr
+#     # normalize
+#     for i in range(nrows):
+#         rsum = 0.0    
+#         for j in range(ptr[i], ptr[i+1]):
+#             rsum += val[j]**2
+#         if rsum == 0.0:
+#             continue  # do not normalize empty rows
+#         rsum = 1.0/np.sqrt(rsum)
+#         for j in range(ptr[i], ptr[i+1]):
+#             val[j] *= rsum
+            
+#     if copy is True:
+#         return mat
+
 def build_matrix_1(docs, labels):
     r""" Build sparse matrix from a list of documents, 
     each of which is a list of word/terms in the document.  
@@ -52,68 +115,72 @@ def build_matrix_1(docs, labels):
     
     return mat
 
+from sklearn.cluster import KMeans
+from numpy import *
+%matplotlib inline
+import matplotlib.pyplot as plt
 
-def csr_info(mat, name="", non_empy=False):
-    r""" Print out info about this CSR matrix. If non_empy, 
-    report number of non-empty rows and cols as well
-    """
-    if non_empy:
-        print("%s [nrows %d (%d non-empty), ncols %d (%d non-empty), nnz %d]" % (
-                name, mat.shape[0], 
-                sum(1 if mat.indptr[i+1] > mat.indptr[i] else 0 
-                for i in range(mat.shape[0])), 
-                mat.shape[1], len(np.unique(mat.indices)), 
-                len(mat.data)))
-    else:
-        print( "%s [nrows %d, ncols %d, nnz %d]" % (name, 
-                mat.shape[0], mat.shape[1], len(mat.data)) )
-        
-# scale matrix and normalize its rows
-def csr_idf(mat, copy=False, **kargs):
-    r""" Scale a CSR matrix by idf. 
-    Returns scaling factors as dict. If copy is True, 
-    returns scaled matrix and scaling factors.
-    """
-    if copy is True:
-        mat = mat.copy()
-    nrows = mat.shape[0]
-    nnz = mat.nnz
-    ind, val, ptr = mat.indices, mat.data, mat.indptr
-    # document frequency
-    df = defaultdict(int)
-    for i in ind:
-        df[i] += 1
-    # inverse document frequency
-    for k,v in df.items():
-        df[k] = np.log(nrows / float(v))  ## df turns to idf - reusing memory
-    # scale by idf
-    for i in range(0, nnz):
-        val[i] *= df[ind[i]]
-        
-    return df if copy is False else mat
 
-def csr_l2normalize(mat, copy=False, **kargs):
-    r""" Normalize the rows of a CSR matrix by their L-2 norm. 
-    If copy is True, returns a copy of the normalized matrix.
-    """
-    if copy is True:
-        mat = mat.copy()
-    nrows = mat.shape[0]
-    nnz = mat.nnz
-    ind, val, ptr = mat.indices, mat.data, mat.indptr
-    # normalize
-    for i in range(nrows):
-        rsum = 0.0    
-        for j in range(ptr[i], ptr[i+1]):
-            rsum += val[j]**2
-        if rsum == 0.0:
-            continue  # do not normalize empty rows
-        rsum = 1.0/np.sqrt(rsum)
-        for j in range(ptr[i], ptr[i+1]):
-            val[j] *= rsum
-            
-    if copy is True:
-        return mat
+def BisectingKMeans(mat4, k_start, k_end, step):
+    for k in range(k_start, k_end+1, step):
+#         print('============================================')
+        X = mat4
+        num_clusters = k
+        k_list = [] 
+        sse_list = [] 
+        total_SSE = 0
+        current_clusters = 1
+
+        clusterMap = {}
+        for idx,row in enumerate(X):
+            clusterMap[idx] = idx
+        finalClusterLabels = {}
+
+        while current_clusters != num_clusters:
+        #     print('final labels', finalClusterLabels)
+        #     print('clusterMap',clusterMap)
+            kmeans = KMeans(n_clusters=2).fit(X)
+    #         print(kmeans.inertia_ )
+            cluster_centers = kmeans.cluster_centers_
+        #     print(X.shape)
+            sse = [0]*2
+            for point, label in zip(X, kmeans.labels_):
+                sse[label] += np.square(point-cluster_centers[label]).sum()
+            chosen_cluster = np.argmax(sse, axis=0)
+            total_SSE += sse[np.argmin(sse, axis=0)]
+    #         print('SSE', sse)
+    #         print('Total SSE', total_SSE)
+    #         print('chosen_cluster', chosen_cluster)
+    #         print('kmeans labels', kmeans.labels_)
+        #     print('cluster_centers', cluster_centers.shape)
+            chosen_cluster_data = X[kmeans.labels_ == chosen_cluster]
+    # 
+            newClusterMap = {}
+            clusterIter = 0
+            for idx, x in enumerate(kmeans.labels_):
+                if(x != chosen_cluster):
+                    finalClusterLabels[clusterMap[idx]] = current_clusters
+                elif current_clusters + 1 == num_clusters:
+                    finalClusterLabels[clusterMap[idx]] = current_clusters + 1
+                else:
+                    newClusterMap[clusterIter] = clusterMap[idx]
+                    clusterIter += 1 
+            clusterMap = newClusterMap
+            current_clusters += 1
+
+    #         print('chosen_cluster_data', chosen_cluster_data.shape)
+            assigned_cluster_data = X[kmeans.labels_ != chosen_cluster]
+    #         print('assigned_cluster_data', assigned_cluster_data.shape)
+            X = chosen_cluster_data
+    #         print('finalClusterLabels - len ', len(finalClusterLabels))
+
+        k_list.append(k)
+        sse_list.append(kmeans.inertia_ )
+        print_internal_metrics(mat4, finalClusterLabels)
+
+    return finalClusterLabels
+#         print(k_list, sse_list)
+
 
 # %%
 #pre-processing
@@ -133,138 +200,75 @@ with open("train.clabel", "r", encoding="utf8") as fh:
 
 
 # %%
+#Internal Metrics
+
+from sklearn import metrics
+
+def print_internal_metrics(mat, labels_dict):
+    labels = [labels_dict[key] for key in sorted(labels_dict.keys())]
+    n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
+
+    print(n_clusters_, metrics.silhouette_score(mat, labels), metrics.calinski_harabasz_score(mat, labels))
+#     print('Estimated number of clusters: %d' % n_clusters_)
+#     print("Silhouette Coefficient: %0.3f" % metrics.silhouette_score(mat, labels))
+#     print("Calinski Harabasz Score: %0.3f" % metrics.calinski_harabasz_score(mat, labels))
+
+
+# %%
 import numpy as np
 from sklearn.datasets import make_blobs
+from sklearn.decomposition import TruncatedSVD
+from sklearn.feature_extraction.text import TfidfTransformer 
+from datetime import datetime
+
 
 with open("train.dat", "r", encoding="utf8") as fh:
     rows = fh.readlines()
 
 mat1 = build_matrix_1(rows, labels)
-print(type(mat1))
-mat2 = csr_idf(mat1, copy=True)
-mat3 = csr_l2normalize(mat2, copy=True)
 
-print(mat3.shape)
-csr_info(mat3)
+tfidf_transformer=TfidfTransformer(smooth_idf=True,use_idf=True) 
+tf_idf_vector=tfidf_transformer.fit(mat1).transform(mat1)
+print('Shape before SVD')
+csr_info(tf_idf_vector)
+
+print("Start Time =", datetime.now().strftime("%H:%M:%S"))
+components = 2500
+while components < 2501:
+    print('============================================')
+    print('SVD number of concepts = ', components)
+    tsvd = TruncatedSVD(n_components=components)
+    mat4 = tsvd.fit(tf_idf_vector).transform(tf_idf_vector)
+    print('Variance ratio sum', tsvd.explained_variance_ratio_.sum())
+#     print(''tsvd.singular_values_.sum())
+#     print('Shape after SVD')
+    csr_info(mat4)
+    finalClusterLabels = BisectingKMeans(mat4, 10, 10, 1)
+    components += 500
+print("End Time =", datetime.now().strftime("%H:%M:%S"))
+
 
 # %%
-from sklearn.cluster import KMeans
-from numpy import *
-%matplotlib inline
-import matplotlib.pyplot as plt
-
-num_clusters = 7
-k_list = [] 
-sse_list = [] 
-
-for k in range(7, 8):
-    total_SSE = 0
-
-    X = mat3
-    clusterMap = {}
-    for idx,row in enumerate(X):
-        clusterMap[idx] = idx
-    finalClusterLabels = {}
-
-    current_clusters = 1
-    while current_clusters != num_clusters:
-        print('============================================')
-    #     print('final labels', finalClusterLabels)
-    #     print('clusterMap',clusterMap)
-        kmeans = KMeans(n_clusters=2).fit(X)
-        print(kmeans.inertia_ )
-        cluster_centers = kmeans.cluster_centers_
-    #     print(X.shape)
-        sse = [0]*2
-        for point, label in zip(X, kmeans.labels_):
-            sse[label] += np.square(point-cluster_centers[label]).sum()
-        chosen_cluster = np.argmax(sse, axis=0)
-        total_SSE += sse[np.argmin(sse, axis=0)]
-        print('SSE', sse)
-        print('Total SSE', total_SSE)
-        print('chosen_cluster', chosen_cluster)
-        print('kmeans labels', kmeans.labels_)
-    #     print('cluster_centers', cluster_centers.shape)
-        chosen_cluster_data = X[kmeans.labels_ == chosen_cluster]
-
-        newClusterMap = {}
-        clusterIter = 0
-        for idx, x in enumerate(kmeans.labels_):
-            if(x != chosen_cluster):
-                finalClusterLabels[clusterMap[idx]] = current_clusters
-            elif current_clusters + 1 == num_clusters:
-                finalClusterLabels[clusterMap[idx]] = current_clusters + 1
-            else:
-                newClusterMap[clusterIter] = clusterMap[idx]
-                clusterIter += 1 
-        clusterMap = newClusterMap
-        current_clusters += 1
-
-        print('chosen_cluster_data', chosen_cluster_data.shape)
-        assigned_cluster_data = X[kmeans.labels_ != chosen_cluster]
-        print('assigned_cluster_data', assigned_cluster_data.shape)
-        X = chosen_cluster_data
-        print('finalClusterLabels - len ', len(finalClusterLabels))
-
-    k_list.append(k)
-    sse_list.append(kmeans.inertia_ )
-        
-print(k_list, sse_list)
-
-
-
-# print(finalClusterLabels)
-# print(mat3)
-# km = KMeans(n_clusters=num_clusters)
-# km.fit(mat3)
+# plt.plot(k_list, sse_list)
+# plt.ylabel('SSE')
+# plt.xlabel('k')
+# plt.show()
 
 # %%
-plt.plot(k_list, sse_list)
-plt.ylabel('SSE')
-plt.xlabel('k')
-plt.show()
 
-# %%
-#Compare with last output
-
-from sklearn import metrics
-from sklearn.metrics.cluster import normalized_mutual_info_score
-
-with open("output.dat", "r", encoding="utf8") as fh:
-    labels_true = fh.readlines()
-
-# Number of clusters in labels, ignoring noise if present.
 labels = [finalClusterLabels[key] for key in sorted(finalClusterLabels.keys())]
-# labels = km.labels_
-# labels_true =  km.labels_.tolist()
-
-n_clusters_ = len(set(labels)) - (1 if -1 in labels else 0)
-print('Estimated number of clusters: %d' % n_clusters_)
-print('NMI: %s' % normalized_mutual_info_score(labels, labels_true))
-print("Homogeneity: %0.3f" % metrics.homogeneity_score(labels_true, labels))
-print("Completeness: %0.3f" % metrics.completeness_score(labels_true, labels))
-print("V-measure: %0.3f" % metrics.v_measure_score(labels_true, labels))
-print("Adjusted Rand Index: %0.3f"
- % metrics.adjusted_rand_score(labels_true, labels))
-print("Adjusted Mutual Information: %0.3f"
- % metrics.adjusted_mutual_info_score(labels_true, labels))
-
-# %%
 with open("output.dat", "w", encoding="utf8") as file:
      for item in labels:
         file.write("%s\n" % str(item))
 len(rows)
 
 # %%
-res = {}
-with open('output.dat', 'r') as resultFile:
-    labels_true = resultFile.readlines()
-    for (row, label) in enumerate(labels_true):  
-
-        if not label in res:
-            res[label] = 1
-        else:
-            res[label] = res[label] + 1
-            
-#         resultFile.write(str(label + 1) + '\n');
-res
+count_labels = {}
+for label in labels:
+    
+    if label not in count_labels:
+        count_labels[label] = 1
+    else:
+        count_labels[label] = int(count_labels[label]) + 1
+#     print(count_labels[label])
+print(count_labels)
